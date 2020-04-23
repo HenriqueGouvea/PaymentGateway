9dd630643017643fc3d81a45ba75ecf02820689d
Henrique Gouvêa <Henrique.Gouvea@malvernpanalytical.com>
2020-04-23 17:59:50 +0100

    Add first version of PaymentController

---
+++ PaymentGateway.API/Controllers/PaymentController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaymentGateway.API.DTO;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.API.Controllers
+{
+  /// <summary>
+  /// Controller responsible for the payment processing and reports.
+  /// </summary>
+  [Route("api/[controller]")]
+  [ApiController]
+  public class PaymentController : Controller
+  {
+    [HttpPost]
+    public async Task<IActionResult> ProcessPayment(PaymentRequest request)
+    {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      // Map to a service object
+
+      // Send asynchronously to the gateway layer
+
+      // Receive an URI from gateway layer
+      var uri = string.Empty;
+
+      request.GenerateNewId();
+
+      // Save the process request in a log system
+
+      return Created(uri, request);
+    }
+  }
+}
---
+++ PaymentGateway.API/DTO/PaymentRequest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PaymentGateway.API.DTO
+{
+  /// <summary>
+  /// The payment processing request object.
+  /// </summary>
+  public class PaymentRequest
+  {
+    public Guid Id { get; private set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    [CreditCard]
+    public string Number { get; set; }
+
+    [Required]
+    [Range(1, 12)]
+    public int ExpiryMonth { get; set; }
+
+    [Required]
+    [Range(2000, 2200)]
+    public int ExpiryYear { get; set; }
+
+    [Required]
+    [Range(0, 999999999)]
+    public decimal Amount { get; set; }
+
+    [Required]
+    public string Currency { get; set; }
+
+    [Required]
+    [Range(100, 9999)]
+    public int CVV { get; set; }
+
+    /// <summary>
+    /// Generates the new identifier to the payment.
+    /// </summary>
+    public void GenerateNewId()
+    {
+      Id = Guid.NewGuid();
+    }
+  }
+}
