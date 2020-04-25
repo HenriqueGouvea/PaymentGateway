4bad104bc118ec7823088e460618714a5253a39a
Henrique Gouvêa <Henrique.Gouvea@malvernpanalytical.com>
2020-04-25 12:56:52 +0100

    Add unit tests for API layer

---
+++ PaymentGateway.API.Tests/Controllers/PaymentControllerTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using PaymentGateway.API.Controllers;
+using PaymentGateway.API.DTO;
+using PaymentGateway.Services.PaymentProcessing;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentGateway.API.Tests.Controllers
+{
+  public class PaymentControllerTests
+  {
+    private PaymentController _target;
+    private Mock<IPaymentProcessingService> _paymentProcessingService;
+
+    [SetUp]
+    public void Setup()
+    {
+      _paymentProcessingService = new Mock<IPaymentProcessingService>();
+      _target = new PaymentController(_paymentProcessingService.Object);
+    }
+
+    [Test]
+    public async Task ProcessPayment_ModelStateInvalid_ReturnsBadRequest()
+    {
+      // Arrange
+      var paymentRequest = new PaymentRequest
+      {
+        Name = string.Empty,
+        Amount = 1,
+        Currency = "EUR",
+        CVV = 123,
+        ExpiryMonth = 12,
+        ExpiryYear = 2021,
+        Number = "4657789945661234"
+      };
+
+      _target.ModelState.AddModelError("Test", "Test error message");
+
+      // Act
+      var result = await _target.ProcessPayment(paymentRequest) as BadRequestObjectResult;
+
+      // Assert
+      Assert.IsNotNull(result);
+      Assert.AreEqual(StatusCodes.Status400BadRequest, result.StatusCode);
+    }
+
+    [Test]
+    public async Task ProcessPayment_ModelStateValid_ReturnsOk()
+    {
+      // Arrange
+      var paymentRequest = new PaymentRequest
+      {
+        Name = "Bill Gates",
+        Amount = 1,
+        Currency = "EUR",
+        CVV = 123,
+        ExpiryMonth = 12,
+        ExpiryYear = 2021,
+        Number = "4657789945661234"
+      };
+
+      var id = Guid.NewGuid();
+      var serviceResponse = new PaymentProcessingResponse(id, "Approved");
+      _paymentProcessingService.Setup(s => s.Process(It.IsAny<PaymentProcessingRequest>())).ReturnsAsync(serviceResponse);
+
+      // Act
+      var result = await _target.ProcessPayment(paymentRequest) as CreatedResult;
+
+      // Assert
+      Assert.IsNotNull(result);
+      Assert.AreEqual(StatusCodes.Status201Created, result.StatusCode);
+      Assert.AreEqual($"https://localhost:44365/api/payment/{id}", result.Location);
+    }
+  }
+}
--- PaymentGateway.API/Controllers/PaymentController.cs
+++ PaymentGateway.API/Controllers/PaymentController.cs
@@ -20,6 +20,11 @@ namespace PaymentGateway.API.Controllers
       _paymentProcessingService = paymentProcessingService;
     }
 
+    /// <summary>
+    /// Process a payment request verifying if it's approved or not by the banking institution.
+    /// </summary>
+    /// <param name="request">The payment request.</param>
+    /// <returns>The payment response and the link to recover the data about the payment processing.</returns>
     [HttpPost]
     public async Task<IActionResult> ProcessPayment(PaymentRequest request)
     {
@@ -32,9 +37,9 @@ namespace PaymentGateway.API.Controllers
       var serviceResponse = await _paymentProcessingService.Process(processPayment);
       var paymentResponse = serviceResponse.ToPaymentResponse(request);
 
-      // Saved in the database
+      // Saved in the database - update unit test
 
-      // Save the process request in a log system
+      // Save the process request in a log system - update unit test
 
       return Created($"https://localhost:44365/api/payment/{paymentResponse.Id}", paymentResponse);
     }
